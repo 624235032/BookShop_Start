f8b5f659fcc91a74c89765c2a9eae02848cb68f8
LAPTOP-20Q48UMK\User <624235032@parichat.skru.ac.th>
2021-10-08 17:00:06 +0700

    first commit

---
+++ src/components/Cart.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <h2>Books in Cart</h2>
+    <div v-bind:key="book.bookid" v-for="book in cart">
+        <CartItem v-bind:book="book" v-on:decrease:cartitem="Decrease"/>
+    </div>
+</div>
+</template>
+
+<script>
+import CartItem from './CartItem.vue';
+export default {
+    name: "Cart",
+    props: ["cart"],
+    components: {
+        CartItem
+    },
+    methods:{
+        Decrease(bookId){
+            this.$emit("decrease:cart",bookId);
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.book-item {
+    background: #e6f2ff;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+}
+</style>
